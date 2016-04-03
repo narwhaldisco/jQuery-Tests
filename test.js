52f62d7eca26e3ae3beacd5b5ff02214f25292e5
Zachary Hoggard <zhoggard9@gmail.com>
2016-04-03 16:19:08 -0400

    Added reliability tests

--- test.js
+++ test.js
@@ -25,3 +25,51 @@ describe('jQuery Tests', function(){
 		});
 	});
 });
+
+describe('jQuery Reliability Tests', function(){
+	
+	for(var i = 0; i < 1000; i++ ) {
+		describe('Test .text() reliability', function(){
+			it('$( "div.demo-container" ).text()', function(){
+
+				//some jQuery!
+				var test = $( "div.demo-container" ).text();
+				
+				//Check functional
+				unitjs
+				  .string(test)
+				  .contains('Demonstration Box');
+			});
+		});
+	}
+
+	for(var i = 0; i < 1000; i++) {
+		describe('Test .text() reliability', function() {
+			it('$("p").val("hello")', function() {
+				$("p").text("My first paragraph.");
+				$("p").text("hello");
+				
+				var test = $("p").text();
+				
+				unitjs
+					.string(test)
+					.contains('hello');
+			});
+		});
+	}
+	
+	for(var i = 0; i < 1000; i++) {
+		describe('Test AJAX get call reliability', function() {
+			it("$.get(http://httpbin.org/)", function() {
+				
+				var test;
+				$.get("http://httpbin.org/", function(data, status) {
+					var test = status;
+					unitjs
+						.string(test)
+						.contains('success');
+				});
+			});
+		});
+	}
+});
